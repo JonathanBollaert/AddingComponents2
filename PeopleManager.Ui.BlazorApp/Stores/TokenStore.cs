fc414ce5fb7c553ee73db5d2816ef9700adf6f96
JonathanBollaert <159426039+JonathanBollaert@users.noreply.github.com>
2024-12-03 15:31:39 +0100

    Initial commit

---
+++ PeopleManager.Ui.BlazorApp/Stores/TokenStore.cs
@@ -0,0 +1,22 @@
+﻿using Blazored.LocalStorage;
+using Vives.Presentation.Authentication;
+
+namespace PeopleManager.Ui.BlazorApp.Stores
+{
+    public class TokenStore(ISyncLocalStorageService localStorageService) : IBearerTokenStore
+    {
+        private readonly ISyncLocalStorageService _localStorageService = localStorageService;
+
+        private const string TokenName = "BearerToken";
+
+        public string GetToken()
+        {
+            return _localStorageService.GetItemAsString(TokenName) ?? string.Empty;
+        }
+
+        public void SetToken(string token)
+        {
+            _localStorageService.SetItem(TokenName, token);
+        }
+    }
+}
